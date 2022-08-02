924b552d2b238fb5e7c6016620a287ab48442e60
Hernan <hernan_carp93@hotmail.com>
2022-08-02 02:14:29 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -218,7 +218,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + nombre + "!"
+  var saludando = "Hola" + nombre + "!"
+  return saludando
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
