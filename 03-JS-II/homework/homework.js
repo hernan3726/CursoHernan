fc4482971288d14044fdb2995c49ab356d96e498
Hernan <hernan_carp93@hotmail.com>
2022-10-05 04:04:12 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -177,7 +177,17 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
+  //Escribe tu código aquí 
+  function tablaDelSeis(){
+    //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+    //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+    //Escribe tu código aquí     
+    let arrayTablaDel6 = []
+    for (let i = 0; i < 11; i++) {
+          arrayTablaDel6.push(6 * i)
+    }
+    return arrayTablaDel6
+  }  
 
 
 function tieneTresDigitos(numero){
